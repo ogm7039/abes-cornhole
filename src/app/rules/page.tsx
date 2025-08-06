5ee45137f44ac5a8550e7fb4d3479f62c0657a65
Owen Manley <ogm7039@rit.edu>
2025-08-06 15:04:20 -0400

    Finished rules page

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -3,6 +3,7 @@
 
 import { Accordion, ListGroup } from 'react-bootstrap';
 import { URL } from 'url';
+import AccordionBody from '../../../node_modules/react-bootstrap/esm/AccordionBody';
 
 export default function RulesPage() {
   return (
@@ -45,7 +46,7 @@ export default function RulesPage() {
                         <li>If a bag touches the ground before landing on the board, it should be removed before scoring.</li>
                         <li>Cancellation Scoring: Only the difference in scores is counted.</li>
                         <li>First team to 21 points wins.</li>
-                        <li>Bust rule: if you go over 21 you reset to 15</li>
+                        <li>Bust rule: if you go over 21 you reset to 15.</li>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
@@ -55,14 +56,29 @@ export default function RulesPage() {
         <Accordion.Item eventKey="1">
           <Accordion.Header>League Rules</Accordion.Header>
           <Accordion.Body>
-            <ListGroup>
-              <li>Games played weekly on Wednesday nights</li>
-              <li>Best of 3 matches per night</li>
-              <li>2–4 players per team (subs allowed)</li>
-              <li>1 point per match win; top 4 teams make playoffs</li>
-              <li>Forfeits = -1 point; 10-minute grace period</li>
-              <li>Fun theme nights, bar specials, and light trash talk encouraged</li>
-            </ListGroup>
+          <Accordion alwaysOpen>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Team Fee</Accordion.Header>
+                    <Accordion.Body>
+                            <li>$100 per team, or $50 a player (to discourage no shows).</li>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Gameplay</Accordion.Header>
+                    <Accordion.Body>
+                        <li>Matches will consist of two teams playing a best two out of three.</li>
+                        <li>Bust rule: if you go over 21, you reset to 15.</li>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Subs</Accordion.Header>
+                    <Accordion.Body>
+                        <li>Teams are allowed to get a sub to fill in for a team member that can’t make it.</li>
+                        <li>Subs can be from other teams.</li>
+                        <li>If there are two teams that only have one player, they may team up and split the points earned between their respective teams.</li>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
           </Accordion.Body>
         </Accordion.Item>
 
@@ -70,12 +86,23 @@ export default function RulesPage() {
           <Accordion.Header>Standings/Points</Accordion.Header>
           <Accordion.Body>
             <ListGroup>
-              <li>Games played weekly on Wednesday nights</li>
-              <li>Best of 3 matches per night</li>
-              <li>2–4 players per team (subs allowed)</li>
-              <li>1 point per match win; top 4 teams make playoffs</li>
-              <li>Forfeits = -1 point; 10-minute grace period</li>
-              <li>Fun theme nights, bar specials, and light trash talk encouraged</li>
+              <Accordion alwaysOpen>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Teams are awarded one point for each game they win in a match</Accordion.Header>
+                    <Accordion.Body>
+                        <li>Winning team gets 2 points no matter what.</li>
+                        <li>If a team forces a game three, they will be awarded 1 point.</li>
+                        <li>(Think of it as similar to the NHL’s 1 point for overtime losses).</li>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Teams are redacted 0.5 points if they forfeit</Accordion.Header>
+                    <Accordion.Body>
+                        <li>Done to discourage no-shows.</li>
+                        <li>Team opposite the forfeit will receive one point.</li>
+                    </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
             </ListGroup>
           </Accordion.Body>
         </Accordion.Item>
@@ -83,14 +110,29 @@ export default function RulesPage() {
         <Accordion.Item eventKey="3">
           <Accordion.Header>Playoff Format</Accordion.Header>
           <Accordion.Body>
-            <ListGroup>
-              <li>Games played weekly on Wednesday nights</li>
-              <li>Best of 3 matches per night</li>
-              <li>2–4 players per team (subs allowed)</li>
-              <li>1 point per match win; top 4 teams make playoffs</li>
-              <li>Forfeits = -1 point; 10-minute grace period</li>
-              <li>Fun theme nights, bar specials, and light trash talk encouraged</li>
-            </ListGroup>
+            <Accordion alwaysOpen>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Each team will make playoffs.</Accordion.Header>
+                    <Accordion.Body>
+                            <li>Just like all the sports we've played, we'll still get participation trophies.</li>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Will use a standard playoff bracket</Accordion.Header>
+                    <Accordion.Body>
+                        <li>Will add a “play in” if we have an odd number of teams.</li>
+                        <li>High seed will play low seed.</li>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Winnings (assuming a 6 team league)</Accordion.Header>
+                    <Accordion.Body>
+                        <li>1st place: $300</li>
+                        <li>2nd place: $200</li>
+                        <li>3rd place: $100</li>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
