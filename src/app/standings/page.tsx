e2e09b0c6d8bb7aa3d3c7560dfea3216d943dd44
Owen Manley <ogm7039@rit.edu>
2025-08-06 15:53:52 -0400

    Added standing data to work with

--- src/app/standings/page.tsx
+++ src/app/standings/page.tsx
@@ -4,12 +4,12 @@ import { useState, useEffect } from "react";
 
 export default function Standings() {
     const standingsData = [
-        {id: 0, name: "Team One", wins: 0, losses: 0, points: 0},
-        {id: 0, name: "Team Two", wins: 0, losses: 0, points: 0},
-        {id: 0, name: "Team Three", wins: 0, losses: 0, points: 0},
-        {id: 0, name: "Team Four", wins: 0, losses: 0, points: 0},
-        {id: 0, name: "Team Five", wins: 0, losses: 0, points: 0},
-        {id: 0, name: "Team Six", wins: 0, losses: 0, points: 0},
+        {id: 0, name: "Team One", wins: 2, losses: 1, points: 5},
+        {id: 0, name: "Team Two", wins: 3, losses: 0, points: 6},
+        {id: 0, name: "Team Three", wins: 1, losses: 2, points: 2},
+        {id: 0, name: "Team Four", wins: 0, losses: 3, points: 3},
+        {id: 0, name: "Team Five", wins: 2, losses: 1, points: 4},
+        {id: 0, name: "Team Six", wins: 1, losses: 2, points: 4},
     ]
 
     return(
